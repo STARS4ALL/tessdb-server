f68603a245d63360090134b9e16d26b9fb7b1e9e
Rafael Gonzalez <astrorafael@yahoo.es>
2016-02-08 19:49:23 +0100

    initial skeleton for windows service, not working

--- tessdb/winserv_main.py
+++ tessdb/winserv_main.py
@@ -1,24 +1,7 @@
 # ----------------------------------------------------------------------
 # Copyright (c) 2014 Rafael Gonzalez.
 #
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-# 
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
-# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
-# LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
-# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
-# WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+# See the LICENSE file for details
 # ----------------------------------------------------------------------
 
 #--------------------
@@ -27,7 +10,6 @@
 
 import os
 import sys
-import logging
 import argparse
 import errno
 
@@ -44,17 +26,21 @@ import win32evtlogutil
 # Twisted imports
 # ---------------
 
+#from twisted.internet import win32eventreactor
+#win32eventreactor.install()
+
+from twisted.internet import reactor
+from twisted.logger import Logger, LogLevel
+
 #--------------
 # local imports
 # -------------
 
-import default
-import cmdline
+import tessdb.logger
 
-from server      import logger
-from emadbserver import EMADBServer
-from default     import VERSION_STRING
-from .           import __version__
+from .  import __version__
+from tessdb.config import VERSION_STRING, cmdline, loadCfgFile
+from tessdb.application import TESSApplication
 
 # ----------------
 # Module constants
@@ -67,74 +53,78 @@ SERVICE_CONTROL_RELOAD = 128
 # Module global variables
 # -----------------------
 
-log = logging.getLogger('winservice')
+log = Logger('tessdb')
 
 
 class WindowsService(win32serviceutil.ServiceFramework):
-    """
-    Windows service for the EMA database.
-    """
-    _svc_name_            = "emadb"
-    _svc_display_name_    = "EMA database %s" % __version__
-    _svc_description_    = "An MQTT Client for EMA weather stations that stores data into a SQLite database"
+	"""
+	Windows service for the EMA database.
+	"""
+	_svc_name_                = "tessdb"
+	_svc_display_name_   = "TESS database {0}".format( __version__)
+	_svc_description_        = "An MQTT Client for TESS that stores data into a SQLite database"
 
-    
-    def __init__(self, args):
-        win32serviceutil.ServiceFramework.__init__(self, args)
-        self.stop    = win32event.CreateEvent(None, 0, 0, None)
-        self.reload  = win32event.CreateEvent(None, 0, 0, None)
-        self.pause  = win32event.CreateEvent(None, 0, 0, None)
-        self.resume = win32event.CreateEvent(None, 0, 0, None)
-        self.server = EMADBServer(cmdline.parser().parse_args(args=args), 
-                                  parent=self,
-                                  stop_event=self.stop,  
-                                  reload_event=self.reload, 
-                                  pause_event=self.pause, 
-                                  resume_event=self.resume)
+	def __init__(self, args):
+		win32serviceutil.ServiceFramework.__init__(self, args)
+		self.stop    = win32event.CreateEvent(None, 0, 0, None)
+		self.reload  = win32event.CreateEvent(None, 0, 0, None)
+		self.pause  = win32event.CreateEvent(None, 0, 0, None)
+		self.resume = win32event.CreateEvent(None, 0, 0, None)
 
-    def SvcStop(self):
-        '''Service Stop entry point'''
-        self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
-        log.info("Stopping emadb %s Windows service", __version__ )
-        logger.sysLogInfo("Stopping emadb %s Windows service" % __version__ )
-        win32event.SetEvent(self.stop)
+	def SvcStop(self):
+		'''Service Stop entry point'''
+		self.ReportServiceStatus(win32service.SERVICE_STOP_PENDING)
+		log.info("Stopping emadb {version} Windows service", version= __version__ )
+		reactor.callFromThread(reactor.stop)
+		logger.sysLogInfo("Stopping emadb %s Windows service" % __version__ )
+		win32event.SetEvent(self.stop)
 
+	def SvcPause(self):
+		'''Service Pause entry point'''
+		self.ReportServiceStatus(win32service.SERVICE_PAUSE_PENDING)
+		log.info("Pausing  emadb {version} Windows service",  version=__version__ )
+		logger.sysLogInfo("Pausing emadb %s Windows service" % __version__ )
+		win32event.SetEvent(self.pause)
+		
+	def SvcContinue(self):
+		'''Service Continue entry point'''
+		self.ReportServiceStatus(win32service.SERVICE_CONTINUE_PENDING)
+		log.info("Resuming emadb %s Windows service", __version__  )
+		logger.sysLogInfo("Resuming emadb %s Windows service" % __version__ )
+		win32event.SetEvent(self.resume)
 
-    def SvcPause(self):
-        '''Service Pause entry point'''
-        self.ReportServiceStatus(win32service.SERVICE_PAUSE_PENDING)
-        log.info("Pausing  emadb %s Windows service",  __version__ )
-        logger.sysLogInfo("Pausing emadb %s Windows service" % __version__ )
-        win32event.SetEvent(self.pause)
-
-        
-    def SvcContinue(self):
-        '''Service Continue entry point'''
-        self.ReportServiceStatus(win32service.SERVICE_CONTINUE_PENDING)
-        log.info("Resuming emadb %s Windows service", __version__  )
-        logger.sysLogInfo("Resuming emadb %s Windows service" % __version__ )
-        win32event.SetEvent(self.resume)
-
-        
-    def SvcOtherEx(self, control, event_type, data):
-        '''Implements a Reload functionality as a  service custom control'''
-        if control == SERVICE_CONTROL_RELOAD:
-            self.SvcDoReload()
-        else:
-            self.SvcOther(control)
-            
-
-    def SvcDoReload(self):
-        logger.sysLogInfo("reloading emadb service")
-        win32event.SetEvent(self.reload)
+	def SvcOtherEx(self, control, event_type, data):
+		'''Implements a Reload functionality as a  service custom control'''
+		if control == SERVICE_CONTROL_RELOAD:
+			self.SvcDoReload()
+		else:
+			self.SvcOther(control)
 
 
-    def SvcDoRun(self):
-        '''Service Run entry point'''
-        logger.sysLogInfo("Starting emadb %s Windows service" % __version__ )
-        self.server.run()
-        self.server.stop()
-        logger.sysLogInfo("emadb %s Windows service stopped" % __version__ )
+	def SvcDoReload(self):
+		logger.sysLogInfo("reloading emadb service")
+		win32event.SetEvent(self.reload)
+
+
+	def SvcDoRun(self):
+		'''Service Run entry point'''
+		logger.sysLogInfo("Starting tessdb %s Windows service {0}".format( __version__ ))
+		# initialize your services here
+		# Read the command line arguments and config file options
+		cmdline_opts = cmdline()
+		if cmdline_opts.config:
+			config_opts  = loadCfgFile(cmdline_opts.config)
+		else:
+			config_opts = None
+
+		# Start the logging subsystem
+		startLogging(console=False, filepath=config_opts['log']['path'])
+
+		sysLogInfo("Starting {0}".format(VERSION_STRING))
+		application = TESSApplication(cmdline_opts, config_opts)
+		application.run()
+		win32event.WaitForSingleObject(self.hWaitStop,win32event.INFINITE)
+		logger.sysLogInfo("tessdb %s Windows service stopped {0}".format( __version__ ))
 
      
 def ctrlHandler(ctrlType):
