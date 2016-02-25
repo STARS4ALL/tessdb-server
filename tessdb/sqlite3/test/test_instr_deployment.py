e197165ba66cec67ec0d72b783d48247b18447f6
Rafael Gonzalez <astrorafael@yahoo.es>
2016-02-26 00:23:11 +0100

    bugfixing test case

--- tessdb/sqlite3/test/test_instr_deployment.py
+++ tessdb/sqlite3/test/test_instr_deployment.py
@@ -28,6 +28,7 @@
 import os
 import sys
 import datetime
+import sqlite3
 
 # ---------------
 # Twisted imports
@@ -36,7 +37,9 @@ import datetime
 from twisted.trial import unittest
 from twisted.test  import proto_helpers
 from twisted.logger   import Logger, LogLevel
+from twisted.internet import reactor
 from twisted.internet.defer import inlineCallbacks, returnValue, succeed
+from twisted.internet.task import deferLater
 
 #--------------
 # local imports
@@ -47,46 +50,34 @@ import tessdb.sqlite3.tess   # to perform one hack
 from   tessdb.error import ReadingKeyError, ReadingTypeError
 from   tessdb.sqlite3 import DBase
 
-
 # Some Sample locations
 TEST_LOCATIONS = [
-    {
-        "location_id"   : 0, 
-        "contact_email" : "asociacion@astrohenares.org", 
-        "site"          : "Centro de Recursos Asociativos El Cerro", 
-        "latitude"      : 40.418561, 
-        "longitude"     : -3.551502, 
-        "elevation"     : 650, 
-        "zipcode"       : '28820', 
-        "location"      : "Coslada", 
-        "province"      : "Madrid", 
-        "country"       : "Spain"
-    }, 
-    {
-        "location_id"   : 1, 
-        "contact_email" : "astroam@gmail.com", 
-        "site"          : "Observatorio Astronomica de Mallorca", 
-        "latitude"      : 39.64269, 
-        "longitude"     : 2.950533, 
-        "elevation"     : 100, 
-        "zipcode"       : '07144', 
-        "location"      : "Costitx", 
-        "province"      : "Mallorca", 
-        "country"       : "Spain"
-    }, 
-]
+        {
+            "location_id"   : 0, 
+            "contact_email" : "asociacion@astrohenares.org", 
+            "site"          : "Centro de Recursos Asociativos El Cerro", 
+            "latitude"      : 40.418561, 
+            "longitude"     : -3.551502, 
+            "elevation"     : 650, 
+            "zipcode"       : '28820', 
+            "location"      : "Coslada", 
+            "province"      : "Madrid", 
+            "country"       : "Spain"
+        }, 
+        {
+            "location_id"   : 1, 
+            "contact_email" : "astroam@gmail.com", 
+            "site"          : "Observatorio Astronomica de Mallorca", 
+            "latitude"      : 39.64269, 
+            "longitude"     : 2.950533, 
+            "elevation"     : 100, 
+            "zipcode"       : '07144', 
+            "location"      : "Costitx", 
+            "province"      : "Mallorca", 
+            "country"       : "Spain"
+        }, 
+    ]
 
-
-TEST_INSTRUMENTS = [
-    { 'name': 'test1', 'mac': '12:34:56:78:90:AB', 'calib': 10.0},
-    { 'name': 'test2', 'mac': '12:34:56:78:90:AC', 'calib': 10.0},
-]
-
-
-TEST_DEPLOYMENTS = [
-    { 'name': 'test1', 'site': 'Centro de Recursos Asociativos El Cerro'},
-    { 'name': 'test2', 'site': 'Observatorio Astronomica de Mallorca'},
-]
 
 def _insertLocations(transaction, rows):
     '''Add new locations'''
@@ -123,6 +114,34 @@ TODAY = datetime.datetime(2016, 02, 21, 12, 00, 00)
 
 class InstrDeployTestCase(unittest.TestCase):
 
+
+    TEST_INSTRUMENTS = [
+        { 'name': 'test1', 'mac': '12:34:56:78:90:AB', 'calib': 10.0},
+        { 'name': 'test2', 'mac': '12:34:56:78:90:AC', 'calib': 10.0},
+        { 'name': 'test2', 'mac': '12:34:56:78:90:AC', 'calib': 15.0},
+        { 'name': 'test3', 'mac': '12:34:56:78:90:AD', 'calib': 10.0},
+        { 'name': 'test3', 'mac': '12:34:56:78:90:AD', 'calib': 17.0},
+        { 'name': 'test4', 'mac': '12:34:56:78:90:AE', 'calib': 10.0},
+    ]
+
+
+    TEST_DEPLOYMENTS1 = [
+        { 'name': 'test1', 'site': 'Centro de Recursos Asociativos El Cerro'},
+        { 'name': 'test2', 'site': 'Observatorio Astronomica de Mallorca'},
+    ]
+
+
+    TEST_DEPLOYMENTS2 = [
+        { 'name': 'test1', 'site': 'Centro Equivocado'},
+        { 'name': 'test2', 'site': 'Observatorio Astronomica de Mallorca'},
+    ]
+
+
+    TEST_DEPLOYMENTS3 = [
+        { 'name': 'wrong-tess1', 'site': 'Centro de Recursos Asociativos El Cerro'},
+        { 'name': 'wrong-tess2', 'site': 'Observatorio Astronomica de Mallorca'},
+    ]
+
     @inlineCallbacks
     def setUp(self):
         try:
@@ -145,17 +164,44 @@ class InstrDeployTestCase(unittest.TestCase):
 
     @inlineCallbacks
     def register(self):
-        for row in TEST_INSTRUMENTS:
+        for row in self.TEST_INSTRUMENTS:
+            yield deferLater(reactor, 1, lambda: None)
             yield self.db.register(row)
 
 
     @inlineCallbacks
-    def test_assign(self):
-        tessdb.sqlite3.tess.DEFAULT_DEPLOYMENT = TEST_DEPLOYMENTS
+    def test_assign_ok(self):
+        tessdb.sqlite3.tess.DEFAULT_DEPLOYMENT = self.TEST_DEPLOYMENTS1
         yield self.db.tess.populate(replace=True)
         rows = yield self.db.pool.runQuery('SELECT name,location_id FROM tess_t ORDER BY name ASC')
         self.assertEqual( rows[0][0], 'test1')
         self.assertEqual( rows[0][1], 0)
         self.assertEqual( rows[1][0], 'test2')
         self.assertEqual( rows[1][1], 1)
-    
+        self.assertEqual( rows[2][0], 'test2')
+        self.assertEqual( rows[2][1], 1)
+        self.assertEqual( rows[3][0], 'test3')
+        self.assertEqual( rows[3][1], -1)
+        self.assertEqual( rows[4][0], 'test3')
+        self.assertEqual( rows[4][1], -1)
+        self.assertEqual( rows[5][0], 'test4')
+        self.assertEqual( rows[5][1], -1)
+
+
+    def test_assign_wrong_loc(self):
+        tessdb.sqlite3.tess.DEFAULT_DEPLOYMENT = self.TEST_DEPLOYMENTS2
+        d = self.db.tess.populate(replace=True)
+        return self.assertFailure(d, sqlite3.IntegrityError)
+
+    @inlineCallbacks
+    def test_assign_wrong_instr(self):
+        tessdb.sqlite3.tess.DEFAULT_DEPLOYMENT = self.TEST_DEPLOYMENTS3
+        d = self.db.tess.populate(replace=True)
+        rows = yield self.db.pool.runQuery('SELECT name,location_id FROM tess_t ORDER BY name ASC')
+        self.assertEqual( rows[0][0], 'test1')
+        self.assertEqual( rows[0][1], 0)
+        self.assertEqual( rows[1][0], 'test2')
+        self.assertEqual( rows[1][1], 1)
+        self.assertEqual( rows[2][0], 'test2')
+        self.assertEqual( rows[2][1], 1)
+
