719e6c6b253102be10b231752e83b94154576fc1
Rafael Gonzalez <astrorafael@yahoo.es>
2016-06-21 19:55:19 +0200

    changed rounding rule and default database resolution

--- tessdb/sqlite3/utils.py
+++ tessdb/sqlite3/utils.py
@@ -49,10 +49,13 @@ log = Logger(namespace='dbase')
 
 def roundDateTime(ts, secs_resol):
    '''Round a timestamp to the nearest minute'''
-   tsround = ts + datetime.timedelta(seconds=0.5*secs_resol)
-   time_id = tsround.hour*10000 + tsround.minute*100 + tsround.second
-   date_id = tsround.year*10000 + tsround.month *100 + tsround.day
-   return date_id, time_id
+    if secs_resol > 1:
+        tsround = ts + datetime.timedelta(seconds=0.5*secs_resol)
+    else:
+        tsround = ts
+    time_id = tsround.hour*10000 + tsround.minute*100 + tsround.second
+    date_id = tsround.year*10000 + tsround.month *100 + tsround.day
+    return date_id, time_id
 
 
 # caveat: this is a blocking I/O operation, but since we are generating
