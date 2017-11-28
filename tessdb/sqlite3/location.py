c983813ff360cdd2c07ae50e2d489acdf65fc7be
Rafael Gonzalez <astrorafael@yahoo.es>
2017-11-28 09:16:12 +0100

    fixed rows returned bug

--- tessdb/sqlite3/location.py
+++ tessdb/sqlite3/location.py
@@ -320,7 +320,7 @@ class Location(Table):
                         'sunset' : sunset,
                         'site'   : site
                     })
-                    return rows
+        return rows
 
 
     @inlineCallbacks
